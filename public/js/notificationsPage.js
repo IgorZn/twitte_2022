439f088c0772b2d8b401e0b19c0f981ffb287cb1
igor.znamensky <igor.znamensky@gmail.com>
2023-03-24 10:48:51 +0300

    Chapter: 29 Done: 255 next: 256

--- public/js/notificationsPage.js
+++ public/js/notificationsPage.js
@@ -1,5 +1,29 @@
 $(document).ready(() => {
     $.get("/api/v1/notifications", results => {
         console.log(results)
+        outputNotificationList(results.data, $(".resultsContainer"))
     })
 })
+
+function outputNotificationList(notifications, container) {
+    notifications.forEach( notification => {
+        const html = createNotificationHtml(notification)
+        container.append(html)
+    })
+
+    if(!notifications.length){
+        container.append("<span class='noResults'>Nothing to show.</span>")
+    }
+}
+
+function createNotificationHtml(notification) {
+    const userFrom = notification.userFrom
+    return `<a href='#' class='resultListItem notification'>
+                <div class='resultsImageContainer'>
+                    <img src='${userFrom.profilePic}'>
+                </div>
+                <div class='resultsDetailsContainer ellipsis'>
+                    <span class='ellipsis'>This is the text</span>
+                </div>
+            </a>`;
+}
